d7d856608efc14b12f86c3d7f7b0c6db649ede7b
root <root@localhost>
2024-08-01 13:12:46 +0530

    auto publish 1722498166291

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct pair{
+
+string first;
+int data;
+
+}p,q,r,s;
+int main() {
+
+ p.first="sayan";
+ p.data=18;
+ q.first="sam";
+ q.data=80;
+ 
+ cout << p.first << endl;
+ 
+ 
+ 
+  return 0;
+    
+}
